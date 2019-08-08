378fb2a0ccf62eaeca1c731079ab70e1d1a08077
sabrinaDz <sabrinasahli@live.fr>
2019-08-08 06:21:25 +0200

    #1 Create DMP Project

---
+++ com.sihatech.DMP/src/test/java/com/sihatech/DMP/DmpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sihatech.DMP;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DmpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
