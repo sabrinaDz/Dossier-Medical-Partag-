6085af0660da03bdd0067149410541c98b16fcfd
sabrinaDz <sabrinasahli@live.fr>
2019-08-13 00:56:24 +0200

    #9 create Crud Service Interface

---
+++ com.sihatech.DMP/src/main/java/com/sihatech/DMP/services/CrudService.java
@@ -0,0 +1,15 @@
+package com.sihatech.DMP.services;
+
+import java.util.Set;
+
+public interface CrudService<T,ID> {
+
+
+	T findById(Long id);
+	Set<T>findAll();
+	T save(T object);
+	void delete(T object);
+	void deleteBydId(Long id);
+
+
+}
